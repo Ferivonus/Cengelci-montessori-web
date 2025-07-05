196da0c2364c2a52598c92fafcdc0f342393deea
Ferivonus <ferivonus@hotmail.com>
2025-07-05 21:57:55 +0300

    Maded betteaahr

--- src/app/montessori-egitimi/page.tsx
+++ src/app/montessori-egitimi/page.tsx
@@ -1,4 +1,5 @@
 // app/montessori-egitimi/page.tsx (örnek)
+import MontessoriEducationPage from "@/components/fully_pages/MontessoriEducationPage";
 import TopNavbar from "@/components/TopNavbar";
 
 export default function MontessoriEgitimi() {
